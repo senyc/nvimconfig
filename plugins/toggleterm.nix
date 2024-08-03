25d64bda38f82edf10e23b1aaf4bdf0ddfc0ea60
kyler <95313103+senyc@users.noreply.github.com>
2024-08-03 10:18:25 -0400

    feat: swap out toggleterm keybinds

--- plugins/toggleterm.nix
+++ plugins/toggleterm.nix
@@ -8,8 +8,18 @@ in {
   keymaps = utils.defaultMap [
     {
       action = ":ToggleTerm direction=float<cr>";
-      key = "<leader>to";
+      key = "<c-;>";
       desc = "Launch floating toggleterm instance";
     }
   ];
+  extraConfigLua = ''
+    -- Toggles (not kills) active terminal session
+    function _G.set_terminal_keymaps()
+      local opts = {noremap = true, silent = true}
+      vim.api.nvim_buf_set_keymap(0, 't', '<esc>', "<cmd>ToggleTermToggleAll<cr>", opts)
+      vim.api.nvim_buf_set_keymap(0, 't', '<c-;>', "<cmd>ToggleTermToggleAll<cr>", opts)
+    end
+
+    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+  '';
 }
