e44bb6093c20d91ab4ad4f754df897ca8b3a23b9
kyler <95313103+senyc@users.noreply.github.com>
2024-11-08 20:47:56 -0500

    feat(treesitter): update go files to syntax highlight sql based on the function a literal string is being used in

--- plugins/treesitter.nix
+++ plugins/treesitter.nix
@@ -13,20 +13,42 @@
     };
   };
   # :h treesitter-language-injections
- 
+
   extraFiles = {
-    "queries/go/injections.scm" = /*query*/
-      ''
-    (
-      (short_var_declaration 
-      left: (expression_list 
+    "queries/go/injections.scm".text =
+      /*query*/''
+        ; If a variable name is query assume it is sql
+        (
+          (short_var_declaration
+            left: (expression_list
               (identifier) @_var_name (#eq? @_var_name "query")
-              ) 
-      right: (expression_list 
-               (raw_string_literal) @injection.content))
-      (#offset! @injection.content 0 1 0 -1)
-      (#set! injection.language "sql")
-    ) 
+              )
+            right: (expression_list
+              [
+                (raw_string_literal)
+                (interpreted_string_literal)
+              ] @injection.content)
+            )
+          (#offset! @injection.content 0 1 0 -1)
+          (#set! injection.language "sql")
+        )
+
+        ; If a function being called is any of the below names assume any string is sql
+        (
+          (call_expression
+            (selector_expression
+              field: (field_identifier) @_field_name (#any-of? @_field_name "Query" "QueryContext" "QueryRowContext" "QueryRow" "Exec" "ExecOne" "ExecContext")
+              )
+            (argument_list
+              [
+                (raw_string_literal)
+                (interpreted_string_literal)
+              ] @injection.content)
+
+            )
+          (#offset! @injection.content 0 1 0 -1)
+          (#set! injection.language "sql")
+        )
       '';
   };
 }
