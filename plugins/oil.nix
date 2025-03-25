c9e4c88a1b0d2c8b7c5d969ce9f33936afc490b5
kyler <95313103+senyc@users.noreply.github.com>
2025-03-25 06:34:35 -0400

    feat(oil): update buffer to display in a float

--- plugins/oil.nix
+++ plugins/oil.nix
@@ -2,18 +2,29 @@ let
   utils = import ../utils.nix;
 in {
   plugins = {
-    oil.enable = true;
+    oil = {
+      enable = true;
+      keymaps = {
+        "<leader>fe" = "actions.close";
+      };
+      settings = {
+        view_options.show_hidden = true;
+        float = {
+          max_height = {
+            __raw = "0.6";
+          };
+          max_width = {
+            __raw = "0.6";
+          };
+        };
+      };
+    };
   };
   keymaps = utils.defaultMap [
     {
-      action = "<cmd>Oil<cr>";
+      action = ":lua require('oil').open_float()<cr>";
       key = "<leader>fe";
       desc = "Starts file explorer (oil)";
-    }
-    {
-      action = "<cmd>vsp | Oil<cr>";
-      key = "<leader>fv";
-      desc = "Starts file explorer (oil) in split";
     }
   ];
 }
