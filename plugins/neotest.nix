17df5aa05dd69b00f67e6cf9c2d625a3702fabc0
kyler <95313103+senyc@users.noreply.github.com>
2025-07-30 00:12:35 -0400

    feat: add neotest keybinds

--- plugins/neotest.nix
+++ plugins/neotest.nix
@@ -1,10 +1,28 @@
-{
+let
+  utils = import ../utils.nix;
+in {
   plugins = {
     neotest = {
       enable = true;
       adapters = {
         plenary.enable = true;
+        go.enable = true;
+        vitest.enable = true;
+        jest.enable = true;
+        python.enable = true;
       };
     };
   };
+  keymaps = utils.defaultMap [
+    {
+      key = "<leader>tr";
+      action = "<cmd>Neotest run<cr>";
+      desc = "Run tests in file";
+    }
+    {
+      key = "<leader>to";
+      action = "<cmd>Neotest summary<cr>";
+      desc = "Open test summary";
+    }
+  ];
 }
