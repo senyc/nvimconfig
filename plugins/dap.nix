263eca568c822e25ccea90a804e708a428fd4ffd
senyc <95313103+senyc@users.noreply.github.com>
2026-01-18 12:50:25 -0500

    feat(dap): update keybinds and add go support

--- plugins/dap.nix
+++ plugins/dap.nix
@@ -3,20 +3,33 @@
 in {
   extraPlugins = [];
   plugins = {
-    dap-virtual-text = {
+    dap-go = {
       enable = true;
       settings = {
-        all_frames = true;
-        all_references = true;
+        delve.path = "${pkgs.delve}/bin/dlv";
       };
     };
+
+    dap-virtual-text = {
+      enable = true;
+    };
+
     dap-ui = {
       enable = true;
     };
+
     dap = {
       enable = true;
       adapters.servers = {
         "pwa-node" = {
+          host = "localhost";
+          port = "\${port}";
+          executable = {
+            command = "${pkgs.vscode-js-debug}/bin/js-debug";
+            args = ["\${port}"];
+          };
+        };
+        "pwa-chrome" = {
           host = "localhost";
           port = "\${port}";
           executable = {
@@ -40,11 +53,13 @@ in {
         dap.listeners.before.event_exited.dapui_config = function()
           ui.close()
         end
-        for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+
+        for _, language in ipairs({ "typescript", "typescriptreact"}) do
             if not dap.configurations[language] then
               dap.configurations[language] = {
+                -- Server-side / Node.js
                 {
-                  name = "Launch (npm run dev:debug)",
+                  name = "Node: Launch (npm run dev:debug)",
                   type = "pwa-node",
                   request = "launch",
                   cwd = vim.fn.getcwd(),
@@ -54,6 +69,52 @@ in {
                   console = "integratedTerminal",
                   skipFiles = { "<node_internals>/**", "**/node_modules/**" },
                   resolveSourceMapLocations = { "''${workspaceFolder}/**", "!**/node_modules/**" },
+                  killBehavior = "forceful",
+                },
+                {
+                  name = "Node: Attach",
+                  type = "pwa-node",
+                  request = "attach",
+                  processId = require("dap.utils").pick_process,
+                  cwd = vim.fn.getcwd(),
+                  sourceMaps = true,
+                  skipFiles = { "<node_internals>/**", "**/node_modules/**" },
+                  killBehavior = "forceful",
+                },
+                {
+                  name = "Node: Attach (Server Components - port 9230)",
+                  type = "pwa-node",
+                  request = "attach",
+                  -- This will start two servers one on 9229 (for Frontend), the other for server 9230
+                  port = 9230,
+                  cwd = vim.fn.getcwd(),
+                  runtimeExecutable = "npm",
+                  args = { "run", "dev:debug" },
+                  sourceMaps = true,
+                  skipFiles = { "<node_internals>/**", "**/node_modules/**" },
+                  resolveSourceMapLocations = { "''${workspaceFolder}/**", "!**/node_modules/**" },
+                  killBehavior = "forceful",
+                },
+                -- Frontend / Browser
+                {
+                  name = "Chrome: Launch (localhost:3000)",
+                  type = "pwa-chrome",
+                  request = "launch",
+                  url = "http://localhost:3000",
+                  webRoot = vim.fn.getcwd(),
+                  sourceMaps = true,
+                  skipFiles = { "<node_internals>/**", "**/node_modules/**" },
+                  killBehavior = "forceful",
+                },
+                {
+                  name = "Chrome: Launch (localhost:5173)",
+                  type = "pwa-chrome",
+                  request = "launch",
+                  url = "http://localhost:5173",
+                  webRoot = vim.fn.getcwd(),
+                  sourceMaps = true,
+                  skipFiles = { "<node_internals>/**", "**/node_modules/**" },
+                  killBehavior = "forceful",
                 },
               }
             end
@@ -63,56 +124,66 @@ in {
   };
 
   keymaps = utils.defaultMap [
-    # this may not work
     {
       key = "<leader>?";
       action = ":lua require('dapui').eval(nil, {enter = true})<cr>";
       desc = "Toggle breakpoint";
     }
     {
-      key = "<leader>bt";
+      key = "<leader>b";
       action = ":lua require('dap').toggle_breakpoint()<cr>";
       desc = "Toggle breakpoint";
     }
     {
-      key = "<leader>bcr";
+      key = "<leader>deb";
+      action = ":lua require('dap').set_exception_breakpoints()<cr>";
+      desc = "Toggle exception breakpoint";
+    }
+    {
+      key = "<leader>drc";
       action = ":lua require('dap').run_to_cursor()<cr>";
-      desc = "Run to cursor";
+      desc = "Debugger run to cursor";
     }
     {
-      key = "<leader>bn";
+      key = "<leader>dc";
       action = ":lua require('dap').continue()<cr>";
-      desc = "Breakpoint next";
+      desc = "debugger continue";
     }
     {
-      key = "<leader>bi";
-      action = ":lua require('dap').step_into()<cr>";
-      desc = "Breakpoint into";
+      key = "<leader>di";
+      action = ":lua require('dap').step_into({askForTargets = true})<cr>";
     }
     {
-      key = "<leader>bo";
+      key = "<leader>duo";
+      action = ":lua require('dapui').open()<cr>";
+      desc = "dap ui open";
+    }
+    {
+      key = "<leader>duc";
+      action = ":lua require('dapui').close()<cr>";
+      desc = "dap ui close";
+    }
+    {
+      key = "<leader>do";
       action = ":lua require('dap').step_over()<cr>";
-      desc = "Breakpoint next";
     }
     {
-      key = "<leader>bx";
+      key = "<leader>dx";
       action = ":lua require('dap').step_out()<cr>";
-      desc = "Breakpoint out";
     }
     {
-      key = "<leader>bb";
+      key = "<leader>db";
       action = ":lua require('dap').step_back()<cr>";
-      desc = "Breakpoint back";
     }
     {
-      key = "<leader>bca";
+      key = "<leader>ca";
       action = ":lua require('dap').clear_breakpoints()<cr>";
-      desc = "Breakpoint back";
+      desc = "Clear all breakpoints";
     }
     {
-      key = "<leader>br";
+      key = "<leader>dr";
       action = ":lua require('dap').restart()<cr>";
-      desc = "Breakpoint restart";
+      desc = "Debugger restart";
     }
   ];
 }
